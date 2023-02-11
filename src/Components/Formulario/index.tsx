f0e8b9355f2f5d99a3c54da94a4252ecc07d25dd
TyphonS <gabriel_silva021@icloud.com>
2023-02-10 22:33:25 -0300

    teste

---
+++ src/Components/Formulario/index.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Botao from "../Botao";
+import'./style.scss';
+
+class Formulario extends React.Component {
+    render() {
+        return(
+        <form className="novaTarefa">
+            <div className="inputContainer">
+                <label htmlFor="tarefa">
+                    Adicione um Novo Estudo
+                </label>
+                <input 
+                type="text"
+                name="tarefa"
+                placeholder="O que você quer estudar"
+                required/>
+            </div>
+
+            <div className="inputContainer">
+                <label htmlFor="tempo">
+                    Tempo
+                </label>
+                <input 
+                type="time"
+                step="1"
+                name="tempo"
+                id="tempo"
+                min="00:00:00"
+                max="01:30:00"
+                required/>
+            </div>
+            <Botao/>            
+        </form>
+        )
+    }
+}
+export default Formulario;
