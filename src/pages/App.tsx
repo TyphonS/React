211f79c54c21a19816452e8888a3b1f26549f22d
TyphonS <gabriel_silva021@icloud.com>
2023-02-22 22:22:26 -0300

    Comit de atualização do codigo seguindo o curso

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Cronometro from '../Components/Cronometro';
 import Formulario from '../Components/Formulario';
 import Lista from '../Components/Lista';
+import { ITarefa } from '../types/tarefa';
 import style from './App.module.scss';
 
 
 function App() {
+  const [tarefas, setTarefas] = useState<ITarefa[]>([]);
+
+  const [selecionado, setSelecionado] = useState<ITarefa>();
+
+  function selecionaTarefa(tarefaSelecionada: ITarefa){
+    setSelecionado(tarefaSelecionada);
+    setTarefas(tarefasAnteriores => tarefasAnteriores.map(tarefa => ({
+      ...tarefa,
+      selecionado: tarefa.id === tarefaSelecionada.id ? true : false
+    })));
+  }
   return (
     <div className={style.AppStyle}>
-      <Formulario />
-      <Lista />
+      <Formulario setTarefas={setTarefas}/>
+      <Lista 
+      tarefas={tarefas}
+      selecionaTarefa={selecionaTarefa}
+      />
       <Cronometro />
     </div>
   );
